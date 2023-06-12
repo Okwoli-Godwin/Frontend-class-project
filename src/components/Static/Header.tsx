e4d2f707fb9c71fbba67214c1845f50bcaf7d80f
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-12 16:18:02 +0100

    update

--- src/components/Static/Header.tsx
+++ src/components/Static/Header.tsx
@@ -1,28 +1,73 @@
 import img from "../../assets/locourse.png"
 import { FiSearch } from "react-icons/fi"
-import {AiOutlineShoppingCart} from "react-icons/ai"
+import { AiOutlineShoppingCart } from "react-icons/ai"
+import {useState, useEffect} from "react"
 
 const Header = () => {
+
+    const [isSticky, setIsSticky] = useState(false)
+
+    useState(() => {
+        const handleScroll = () => {
+            const scroll = window.pageYOffset;
+            if (scroll > 150) {
+                setIsSticky(true)
+            } else {
+                setIsSticky(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    })
+    
   return (
-      <div className="h-20 w-full bg-[#FEF0EF] flex justify-center">
+      <>
+          {isSticky ? (
+              <div className="h-20 w-full bg-[#fff] flex justify-center backdrop-filter backdrop-blur fixed animate-slideDown">
           <div className="w-[95%] flex items-center justify-between">
               <img src={img} className="h-[110px]" />
               
               <div className="flex items-center">
-                  <div className="text-base ml-14 font-medium text-[#5F5982]">Home</div>
-                  <div className="text-base ml-14 font-medium text-[#5F5982]">Courses</div>
-                  <div className="text-base ml-14 font-medium text-[#5F5982]">Features</div>
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Home</div>
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Courses</div>
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Features</div>
               </div>
 
               <div className="flex items-center">
-                  <div className="text-[#5F5982] text-xl ml-8 cursor-pointer"><FiSearch /></div>
-                  <div className="text-[#5F5982] text-xl ml-8 cursor-pointer"><AiOutlineShoppingCart /></div>
+                  <div className="text-[#5F5982] text-2xl ml-10 cursor-pointer"><FiSearch /></div>
+                  <div className="text-[#5F5982] text-2xl ml-10 cursor-pointer"><AiOutlineShoppingCart /></div>
                   <button className="text-white w-[100px] h-[45px] flex justify-center items-center cursor-pointer bg-[#dc3545] rounded ml-8">
                       Register
                   </button>
               </div>
           </div>
     </div>
+          ) : (
+                  <div className="h-20 w-full bg-[#FEF0EF] flex justify-center">
+          <div className="w-[95%] flex items-center justify-between">
+              <img src={img} className="h-[110px]" />
+              
+              <div className="flex items-center">
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Home</div>
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Courses</div>
+                  <div className="text-base ml-16 font-medium text-[#5F5982]">Features</div>
+              </div>
+
+              <div className="flex items-center">
+                  <div className="text-[#5F5982] text-2xl ml-10 cursor-pointer"><FiSearch /></div>
+                  <div className="text-[#5F5982] text-2xl ml-10 cursor-pointer"><AiOutlineShoppingCart /></div>
+                  <button className="text-white w-[100px] h-[45px] flex justify-center items-center cursor-pointer bg-[#dc3545] rounded ml-8">
+                      Register
+                  </button>
+              </div>
+          </div>
+    </div>
+          )}
+      </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideDown: {
+          '0%': {
+            transform: 'translateY(-100%)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1'
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
