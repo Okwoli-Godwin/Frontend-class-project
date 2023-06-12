fcabaced76b826aa92776643fa1254bad90acbb2
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-12 15:29:48 +0100

    update

--- src/components/Static/Header.tsx
+++ src/components/Static/Header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
   return (
       <div className="h-20 w-full bg-[#FEF0EF] flex justify-center">
           <div className="w-[95%] flex items-center">
-              <img src={img}  />
+              <img src={img} className="h-[110px]" />
+              
+              <div className="flex items-cente">
+                  <div className="text-base ml-14 font-medium">Home</div>
+                  <div className="text-base ml-14 font-medium">Courses</div>
+                  <div className="text-base ml-14 font-medium">Features</div>
+              </div>
           </div>
     </div>
   )
