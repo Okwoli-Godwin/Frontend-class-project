a17ab93b5055a16c1d785e0f1d3335bdd5ff3ec0
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-12 17:18:09 +0100

    pushed

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -4,8 +4,9 @@ const Hero = () => {
   return (
       <div className='w-full h-[100%] bg-[#FEF0EF] flex justify-center'>
           <div className="w-[90%] flex">
-              <div className="w-[50%] flex column">
-                <h2 className="text-6xl font-semibold mt-[150px] leading-none text-[#212529]">  Learn more and make success the result of perfection.</h2>
+              <div className="w-[50%] flex flex-col">
+                  <h2 className="text-6xl font-semibold mt-[150px] leading-none text-[#212529]">  Learn more and make success the result of perfection.</h2>
+                  <p className="mt-8 text-xl text-[#5F5982]">Pick from over 100,000 online video courses with new additions published every month.</p>
               </div>
           </div>
     </div>
