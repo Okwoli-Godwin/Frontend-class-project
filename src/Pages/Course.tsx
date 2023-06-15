68057cfcdc7aee006de7db4da4e2a5f05b5addb3
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 13:57:25 +0100

    update

--- src/Pages/Course.tsx
+++ src/Pages/Course.tsx
@@ -2,13 +2,20 @@
 
 const Course = () => {
   return (
-    <div className="w-[100%] bg-[#EFEFF6] justify-center flex">
+    <div className="w-[100%] bg-[#ececffda] justify-center flex">
       <div className="w-[90%] flex flex-col">
+
         <div className="w-full flex h-20 mt-[90px] justify-between">
           <h1 className="text-[43px]">Get choice of your course</h1>
           <button className="text-white w-[100px] h-[45px] flex justify-center items-center cursor-pointer bg-[#dc3545] rounded ml-8">
              View all
           </button>
+        </div>
+
+        <div className="w-full flex">
+          <div className="w-[380px] h-20 bg-white mt-[50px] rounded-md">
+
+          </div>
         </div>
       </div>
     </div>
