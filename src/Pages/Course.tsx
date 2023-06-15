265c85455d1b20937c77b212737bee8d5e1e0756
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 13:48:37 +0100

    update

--- src/Pages/Course.tsx
+++ src/Pages/Course.tsx
@@ -5,7 +5,10 @@ const Course = () => {
     <div className="w-[100%] bg-[#EFEFF6] justify-center flex">
       <div className="w-[90%] flex flex-col">
         <div className="w-full flex h-20 mt-[90px] justify-between">
-          <h1 className="">Get choice of your course</h1>
+          <h1 className="text-[43px]">Get choice of your course</h1>
+          <button className="text-white w-[100px] h-[45px] flex justify-center items-center cursor-pointer bg-[#dc3545] rounded ml-8">
+             View all
+          </button>
         </div>
       </div>
     </div>
