6f2bf533cd8ba741e92788ab995f0c6f8fadf47a
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 16:32:48 +0100

    update

--- src/Pages/Online.tsx
+++ src/Pages/Online.tsx
@@ -2,9 +2,15 @@
 
 const Online = () => {
   return (
-      <div className="w-[100%] flex bg-[#FEF0EF] ">
+      <div className="w-[100%] flex bg-[#FEF0EF] pb-[140px]">
           <div className="w-[90%] flex justify-center items-center relative flex-col">
-              <h1 className="text-[35px] mt-[140px]">Select the best from various online courses.</h1>
+              <h1 className="text-[41px] mt-[140px]">Select the best from various online courses.</h1>
+              <p className="w-[60%] text-[20px] text-center mt-[14px]">
+                  Our online video courses give you the freedom to learn remotely, and our experts will guide you properly.
+              </p>
+              <button className="mt-[25px] text-white w-[100px] h-[45px] flex justify-center items-center cursor-pointer bg-[#dc3545] rounded ml-8">
+                      Join Today
+                </button>
           </div>
     </div>
   )
