d439dc6cc1cb9294e341eb5bed729cd66ba5a25e
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 13:41:45 +0100

    update

--- src/Pages/Course.tsx
+++ src/Pages/Course.tsx
@@ -2,8 +2,10 @@
 
 const Course = () => {
   return (
-    <div className="w-[100%] h-[100vh] bg-[#EFEFF6]">
+    <div className="w-[100%] bg-[#EFEFF6] justify-center flex">
+      <div className="w-[90%] flex flex-col bg-red-400 h-20">
 
+      </div>
     </div>
   )
 }
