b67382f20c64147347e8f81a12b8c1dd0c716382
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 13:45:22 +0100

    update

--- src/Pages/Course.tsx
+++ src/Pages/Course.tsx
@@ -3,8 +3,10 @@
 const Course = () => {
   return (
     <div className="w-[100%] bg-[#EFEFF6] justify-center flex">
-      <div className="w-[90%] flex flex-col bg-red-400 h-20">
-
+      <div className="w-[90%] flex flex-col">
+        <div className="w-full flex h-20 mt-[90px] justify-between">
+          <h1 className="">Get choice of your course</h1>
+        </div>
       </div>
     </div>
   )
