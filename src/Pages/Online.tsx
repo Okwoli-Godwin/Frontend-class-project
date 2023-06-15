31d452fbcda49ade5c3df5c41a2b58597b2ea87a
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 16:43:32 +0100

    update

--- src/Pages/Online.tsx
+++ src/Pages/Online.tsx
@@ -1,12 +1,15 @@
 import dot from "../assets/dots.svg"
+import circ from "../assets/circ.svg"
 
 const Online = () => {
   return (
       <div className="w-[100%] flex bg-[#FEF0EF] pb-[140px] relative">
           <img src={dot} className="absolute bottom-0" />
           <img src={dot} className="absolute right-0 top-[80px]" />
-          <div className="w-[90%] flex justify-center items-center relative flex-col">
-              <h1 className="text-[41px] mt-[140px]">Select the best from various online courses.</h1>
+          <img src={circ} className="absolute right-[70px] top-[80px]" />
+
+          <div className="w-[100%] flex items-center relative flex-col">
+              <h1 className="text-[41px] mt-[150px]">Select the best from various online courses.</h1>
               <p className="w-[60%] text-[20px] text-center mt-[14px]">
                   Our online video courses give you the freedom to learn remotely, and our experts will guide you properly.
               </p>
