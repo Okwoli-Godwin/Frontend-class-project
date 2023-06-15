76a2549fe837d089203f5605087718424e1b22e6
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-15 16:24:53 +0100

    update

--- src/Pages/Online.tsx
+++ src/Pages/Online.tsx
@@ -4,7 +4,7 @@ const Online = () => {
   return (
       <div className="w-[100%] flex bg-[#FEF0EF] ">
           <div className="w-[90%] flex justify-center items-center relative flex-col">
-              <h1>Select the best from various online courses.</h1>
+              <h1 className="text-[35px] mt-[140px]">Select the best from various online courses.</h1>
           </div>
     </div>
   )
